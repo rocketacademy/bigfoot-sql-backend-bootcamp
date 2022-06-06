2d7013ea89223bd3ee2bf627638a429a92b3da62
neokaiyuan <kai@rocketacademy.co>
2022-06-06 23:54:12 +0800

    Add seeder file for categories

---
+++ db/seeders/20220606155008-categories.js
@@ -0,0 +1,72 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Categories", [
+      {
+        name: "Dark sky",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Bright sky",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Cloudy",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Sunny",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Rainy",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Mountain",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Woods",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "City",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Town",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Near (<10m)",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Mid-distance (<100m)",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Far (>100m)",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Categories", null, {});
+  },
+};
