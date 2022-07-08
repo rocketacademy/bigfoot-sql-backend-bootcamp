b7091b85dd0ce525bd88bdb6b0434faf8482fc6f
FoongL <leung.foong@gmail.com>
2022-07-08 18:11:17 +0800

    fixed model names

--- db/models/index.js
+++ db/models/index.js
@@ -22,7 +22,7 @@ if (config.use_env_variable) {
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-4) === ".js"
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
     );
   })
   .forEach((file) => {
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 require('dotenv').config()
 
 const db = require('./db/models/index.js')
-const { Sighting } = db;
+const { sighting } = db;
 
 const PORT = process.env.PORT;
 const app = express();
@@ -12,13 +12,13 @@ const app = express();
 app.use(cors());
 
 app.get("/sightings", async (req, res) => {
-  const sightings = await Sighting.findAll();
+  const sightings = await sighting.findAll();
   res.json(sightings);
 });
 
 app.get("/sightings/:sightingId", async (req, res) => {
   const { sightingId } = req.params
-  const sighting = await Sighting.findByPk(sightingId);
+  const sighting = await sighting.findByPk(sightingId);
   res.json(sighting);
 });
 
