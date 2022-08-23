22deb01a6f16b62564787cd8d55765848a754669
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2022-08-23 15:02:42 +0800

    fixing router

--- routers/sightingsRouter.js
+++ routers/sightingsRouter.js
@@ -1,19 +1,25 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-class CategoriesRouter {
-    constructor(controller){
-        this.controller = controller
-    }
-    routes(){
-        // we will insert routes into here later on
-        router.get('/', this.controller.getAll.bind(this.controller))
-        router.post('/', this.controller.insertOne.bind(this.controller))
-        router.get('/:sightingId', this.controller.getOne.bind(this.controller))
-        router.get('/:sightingId/comments', this.controller.getComments.bind(this.controller))
-        router.post('/:sightingId/comments', this.controller.insertOneComment.bind(this.controller))
-        return router
-    }
+class SightingsRouter {
+  constructor(controller) {
+    this.controller = controller;
+  }
+  routes() {
+    // we will insert routes into here later on
+    router.get("/", this.controller.getAll.bind(this.controller));
+    router.post("/", this.controller.insertOne.bind(this.controller));
+    router.get("/:sightingId", this.controller.getOne.bind(this.controller));
+    router.get(
+      "/:sightingId/comments",
+      this.controller.getComments.bind(this.controller)
+    );
+    router.post(
+      "/:sightingId/comments",
+      this.controller.insertOneComment.bind(this.controller)
+    );
+    return router;
+  }
 }
 
-module.exports = CategoriesRouter
+module.exports = SightingsRouter;
