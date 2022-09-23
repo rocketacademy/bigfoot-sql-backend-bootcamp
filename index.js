c267cec7ef0bfe51789f56a78ae8848eee8a2fa1
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2022-09-23 13:37:49 +0800

    fixed posting issue

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-const cors = require('cors')
-const express = require('express')
-require('dotenv').config()
-
+const cors = require("cors");
+const express = require("express");
+require("dotenv").config();
 
 // importing Routers
-const SightingsRouter = require('./routers/sightingsRouter')
+const SightingsRouter = require("./routers/sightingsRouter");
 
 // importing Controllers
-const SightingsController = require('./controllers/sightingsController')
+const SightingsController = require("./controllers/sightingsController");
 
 // importing DB
-const db = require('./db/models/index')
+const db = require("./db/models/index");
 const { comment, sighting } = db;
 
 // initializing Controllers -> note the lowercase for the first word
-const sightingsController = new SightingsController(sighting, comment)
+const sightingsController = new SightingsController(sighting, comment);
 
 // inittializing Routers
-const sightingRouter = new SightingsRouter(sightingsController).routes()
-
+const sightingRouter = new SightingsRouter(sightingsController).routes();
 
 const PORT = process.env.PORT;
 const app = express();
 
 // Enable CORS access to this server
 app.use(cors());
+app.use(express.json());
 
 // using the routers
-app.use('/sightings', sightingRouter)
+app.use("/sightings", sightingRouter);
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
--- routers/sightingsRouter.js
+++ routers/sightingsRouter.js
@@ -8,8 +8,7 @@ class SightingsRouter {
   routes() {
     // we will insert routes into here later on
     router.get("/", this.controller.getAll.bind(this.controller));
-    router.use(express.urlencoded({ extended: true }));
-    router.post("/sightings", this.controller.insertOne.bind(this.controller));
+    router.post("/", this.controller.insertOne.bind(this.controller));
     router.get("/:sightingId", this.controller.getOne.bind(this.controller));
     return router;
   }
