83578d1dded70c443cc500d49439b98f31863bb7
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2022-10-14 18:48:01 +0800

    this code creates an error with posting

--- controllers/sightingsController.js
+++ controllers/sightingsController.js
@@ -7,9 +7,8 @@ class SightingsController extends BaseController {
 
   //Create sighting
   async insertOne(req, res) {
-    console.log(req.body);
     const { date, location, notes } = req.body;
-
+    console.log(date);
     try {
       // Create new sighting
       const newSighting = await this.model.create({
@@ -19,6 +18,7 @@ class SightingsController extends BaseController {
       });
       // Respond with new sighting
       console.log(newSighting);
+      // return res.send("Hello cutie!");
       return res.json(newSighting);
     } catch (err) {
       console.log(err);
@@ -32,6 +32,34 @@ class SightingsController extends BaseController {
     try {
       const sighting = await this.model.findByPk(sightingId);
       return res.json(sighting);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+
+  // Retrieve all comments for specific sighting
+  async getComments(req, res) {
+    const { sightingId } = req.params;
+    try {
+      const comments = await this.commentModel.findAll({
+        where: { sightingId: sightingId },
+      });
+      return res.json(comments);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+
+  // Create comment for specific sighting
+  async insertOneComment(req, res) {
+    const { sightingId } = req.params;
+    const { content } = req.body;
+    try {
+      const newComment = await this.commentModel.create({
+        content: content,
+        sightingId: sightingId,
+      });
+      return res.json(newComment);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
     }
--- db/models/comment.js
+++ db/models/comment.js
@@ -1,28 +1,26 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Sighting extends Model {
+  class comment extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-      this.hasMany(models.comment);
+      //define associations here
+      this.belongsTo(models.sighting);
     }
   }
-  Sighting.init(
+  comment.init(
     {
-      date: DataTypes.DATE,
-      location: DataTypes.STRING,
-      notes: DataTypes.TEXT,
+      content: DataTypes.STRING,
+      sighting_id: DataTypes.INTEGER,
     },
     {
       sequelize,
-      modelName: "sighting",
-      underscored: true,
+      modelName: "comment",
     }
   );
-  return Sighting;
+  return comment;
 };
