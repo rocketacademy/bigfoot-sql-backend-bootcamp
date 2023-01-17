c2511d9589a7178de8938054685134e8ef47f0e9
sharlearn <liewsharlen@gmail.com>
2023-01-18 01:54:12 +0800

    Updated names and added M-M reference in sighting

--- db/models/comment.js
+++ db/models/comment.js
@@ -1,31 +1,32 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class comment extends Model {
+  class Comment extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsTo(models.sighting)
+      this.belongsTo(models.sighting);
     }
   }
-  comment.init({
-    content: DataTypes.STRING,
-    sighting_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "sightings",
-        key: "id"
-      }
+  Comment.init(
+    {
+      content: DataTypes.STRING,
+      sighting_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "sightings",
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "comment",
+      underscored: true,
     }
-  }, {
-    sequelize,
-    modelName: 'comment',
-    underscored: true
-  });
-  return comment;
+  );
+  return Comment;
 };
--- db/models/sighting.js
+++ db/models/sighting.js
@@ -1,26 +1,28 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class sighting extends Model {
+  class Sighting extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasMany(models.comment)
+      this.hasMany(models.comment);
+      this.belongsToMany(models.category, { through: "sighting_categories" });
     }
   }
-  sighting.init({
-    date: DataTypes.DATE,
-    location: DataTypes.STRING,
-    notes: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'sighting',
-    underscored: true,
-  });
-  return sighting;
+  Sighting.init(
+    {
+      date: DataTypes.DATE,
+      location: DataTypes.STRING,
+      notes: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "sighting",
+      underscored: true,
+    }
+  );
+  return Sighting;
 };
