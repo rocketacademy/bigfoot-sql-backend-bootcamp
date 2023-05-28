a6e1f3180a1aa3164d91e23040a62cc3d95c8927
hengmhs <hengmhs@gmail.com>
2023-05-28 15:02:04 +0800

    Added 1-M relationship between sightings and comments in model and migration files

--- db/models/comment.js
+++ db/models/comment.js
@@ -1,30 +1,32 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Sighting extends Model {
+  class comment extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-      this.hasMany(models.comment, {
-        foreignKey: "sighting_id",
-      });
+      this.belongsTo(models.sighting);
     }
   }
-  Sighting.init(
+  comment.init(
     {
-      date: DataTypes.DATE,
-      location: DataTypes.STRING,
-      notes: DataTypes.TEXT,
+      content: DataTypes.STRING,
+      sighting_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "sighting",
+          key: "id",
+        },
+      },
     },
     {
       sequelize,
-      modelName: "sighting",
+      modelName: "comment",
       underscored: true,
     }
   );
-  return Sighting;
+  return comment;
 };
