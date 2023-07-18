e6a5099331090cda4ad85644e3066af99d2bb6d4
ryanlerler <lyp2726@outlook.com>
2023-07-18 21:30:44 +0800

    Update /sightings to retrieve only the filtered data using URL query parameters

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -7,8 +7,17 @@ class BaseController {
 
   getAll = async (req, res) => {
     try {
-      const output = await this.model.findAll();
-      return res.json(output);
+      if (req.query.year) {
+        console.log(typeof req.query.year);
+        const output = await this.model.findAll();
+        const filteredOutput = output.filter((item) =>
+          item.date.toString().includes(req.query.year)
+        );
+        return res.json(filteredOutput);
+      } else {
+        const output = await this.model.findAll();
+        return res.json(output);
+      }
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
     }
