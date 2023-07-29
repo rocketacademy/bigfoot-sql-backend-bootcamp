96ced0e4f1693fe5cf22b595e8de3a98c2b0c981
Huang Yu Hao <yuhao.huang@ntu.edu.sg>
2023-07-29 12:06:54 +0800

    done post for backend

--- controllers/sightingsController.js
+++ controllers/sightingsController.js
@@ -15,6 +15,38 @@ class SightingsController extends BaseController {
       return res.status(400).json({ error: true, msg: err });
     }
   }
+  // Code to insert data
+  async postOne(request, response) {
+    console.log("post request came in");
+    console.log(request.body); // All of the data that is being sent
+
+    // Example body:
+    // {
+    // "date": "2023-09-19",
+    // "location": "Outworld",
+    // "notes": "Met something with feet not only big, but has 4 of them in the Mortal Kombat tournament"
+    // }
+
+    const sighting_date = request.body.date;
+    const location = request.body.location;
+    const notes = request.body.notes;
+
+    try {
+      // Create a new instance of the Sighting model with the data to be inserted
+      const newSighting = this.model.create({
+        date: sighting_date,
+        location: location,
+        notes: notes,
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+
+      // Return the newly created sighting as a response
+      return response.json(newSighting);
+    } catch (err) {
+      return response.status(400).json({ error: true, msg: err });
+    }
+  }
 }
 
 module.exports = SightingsController;
