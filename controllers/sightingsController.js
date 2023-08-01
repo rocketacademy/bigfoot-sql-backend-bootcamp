255de80a41f620764dbf7372f3b4522019105142
alaynelxr <alaynelxr@gmail.com>
2023-08-01 21:01:37 +0800

    updated controller and fixed bind undefined error

--- controllers/sightingsController.js
+++ controllers/sightingsController.js
@@ -5,6 +5,23 @@ class SightingsController extends BaseController {
     super(model);
   }
 
+  // Create sighting
+  async insertOne(req, res) {
+    const { date, location, notes } = req.body;
+    try {
+      // Create new sighting
+      const newSighting = await this.model.create({
+        date: new Date(date),
+        location: location,
+        notes: notes,
+      });
+      // Respond with new sighting
+      return res.json(newSighting);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+
   // Retrieve specific sighting
   async getOne(req, res) {
     const { sightingId } = req.params;
--- routers/sightingsRouter.js
+++ routers/sightingsRouter.js
@@ -6,9 +6,10 @@ class SightingsRouter {
     this.controller = controller;
   }
   routes() {
-    // we will insert routes into here later on
     router.get("/", this.controller.getAll.bind(this.controller));
+    router.post("/new", this.controller.insertOne.bind(this.controller));
     router.get("/:sightingId", this.controller.getOne.bind(this.controller));
+
     return router;
   }
 }
