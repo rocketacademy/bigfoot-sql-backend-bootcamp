c150ef8428aeaaac604d9302358114c989d3ef98
KenHo95 <mingquan.ho.2017@accountancy.smu.edu.sg>
2023-08-08 20:41:46 +0800

    Added migration and model for category and sighting category junction table

--- db/models/category.js
+++ db/models/category.js
@@ -1,7 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Sighting extends Model {
+  class category extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -9,24 +9,18 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.comment);
-      this.hasMany(models.like);
-      this.belongsToMany(models.category, { through: "sighting_categories" });
+      this.belongsToMany(models.sighting, { through: "sighting_categories" });
     }
   }
-  Sighting.init(
+  category.init(
     {
-      date: DataTypes.DATE,
-      location_discription: DataTypes.STRING,
-      notes: DataTypes.TEXT,
-      city: DataTypes.STRING,
-      country: DataTypes.STRING,
+      name: DataTypes.STRING,
     },
     {
       sequelize,
-      modelName: "sighting",
+      modelName: "category",
       underscored: true,
     }
   );
-  return Sighting;
+  return category;
 };
