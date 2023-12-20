7463c50da93b2eb5bc03bb33304cedaf7b07cb07
ianlauyin <ianlauyin@gmail.com>
2023-12-20 15:21:55 +0800

    Create new table Like

---
+++ db/migrations/20231220071900-create-like.js
@@ -0,0 +1,31 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("likes", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      sighting_id: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: "sightings",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("likes");
+  },
+};
---
+++ db/models/like.js
@@ -0,0 +1,25 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class like extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  like.init(
+    {
+      sightingId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "like",
+      underscored: true,
+    }
+  );
+  return like;
+};
