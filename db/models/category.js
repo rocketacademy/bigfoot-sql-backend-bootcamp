bae45c979f14cd3d10c536b9765748d734729909
ianlauyin <ianlauyin@gmail.com>
2023-12-24 11:50:11 +0800

    Create category table

---
+++ db/models/category.js
@@ -0,0 +1,25 @@
+"use strict";
+const { Model, STRING } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsToMany(models.sighting, { through: "sighting_categories" });
+    }
+  }
+  Category.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "category",
+      underscored: true,
+    }
+  );
+  return Category;
+};
--- db/models/sighting.js
+++ db/models/sighting.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       this.hasMany(models.comment);
       this.hasMany(models.like);
+      this.belongsToMany(models.category, { through: "sighting_categories" });
     }
   }
   Sighting.init(
