279b1d0af62d5f830531368a44f528834cdc6a19
ianlauyin <ianlauyin@gmail.com>
2023-12-26 15:11:24 +0800

    Add intensity in table

---
+++ db/migrations/20231226070353-Add-intensity.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn("sighting_categories", "intensity", {
+      type: Sequelize.STRING,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("sighting_categories");
+  },
+};
