2a0ac81232b2b6cb87f68873f400f8dca2111fb8
estney <esther.pxy@gmail.com>
2024-01-24 18:16:54 +0800

    comment table created

---
+++ db/models/comment.js
@@ -0,0 +1,36 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // sighting (one) to many (comment)
+      // comment is many
+      this.belongsTo(models.sighting);
+    }
+  }
+  comment.init(
+    {
+      content: DataTypes.STRING,
+      //captial S in singing.js
+      SightingId: {
+        type: DataTypes.INTEGER,
+        references: {
+          //table must be plurul
+          model: "sightings",
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "comment",
+      underscored: true,
+    }
+  );
+  return comment;
+};
--- db/models/sighting.js
+++ db/models/sighting.js
@@ -7,8 +7,10 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate() {
+    static associate(models) {
       // define association here
+      // one to many
+      this.hasMany(models.comment);
     }
   }
   Sighting.init(
