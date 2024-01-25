9bb0b8d3a2d916ec41002871e97ee283c7439c82
estney <esther.pxy@gmail.com>
2024-01-25 17:34:48 +0800

    created category

---
+++ db/models/category.js
@@ -0,0 +1,26 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsToMany(models.sighting, { through: "sighting_categories" });
+    }
+  }
+  Category.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "category",
+      underscored: true,
+    }
+  );
+  return Category;
+};
