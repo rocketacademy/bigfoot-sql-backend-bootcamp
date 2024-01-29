27be0a7921551cca7d1a9a6af0770f5f2d0933ae
estney <esther.pxy@gmail.com>
2024-01-29 15:27:22 +0800

    commited base m-m

--- controllers/sightingsController.js
+++ controllers/sightingsController.js
@@ -15,7 +15,9 @@ class SightingsController extends BaseController {
   async getOne(req, res) {
     const { sightingId } = req.params;
     try {
-      const sighting = await this.model.findByPk(sightingId);
+      const sighting = await this.model.findByPk(sightingId, {
+        include: this.categoryModel,
+      });
       return res.json(sighting);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
@@ -23,18 +25,45 @@ class SightingsController extends BaseController {
   }
 
   // add sightings
+  // add = async (req, res) => {
+  //   //pass in categories id too for junction table
+  //   const { date, location, notes } = req.body;
+  //   try {
+  //     const newSighting = await this.model.create({
+  //       date: date,
+  //       location: location,
+  //       notes: notes,
+  //     });
+  //     // Respond to the user
+  //     return res.send(newSighting);
+  //   } catch (error) {
+  //     console.error("Error adding sighting:", error);
+  //   }
+  // };
+
+  // add sightings with categories
   add = async (req, res) => {
-    const { date, location, notes } = req.body;
+    //base on frontend naming convention
+    const { date, location, notes, selectedCategoryIds } = req.body;
     try {
+      // Create new sighting
       const newSighting = await this.model.create({
-        date: date,
+        date: new Date(date),
         location: location,
         notes: notes,
       });
-      // Respond to the user
-      return res.send(newSighting);
-    } catch (error) {
-      console.error("Error adding sighting:", error);
+      // Retrieve selected categories
+      const selectedCategories = await this.categoryModel.findAll({
+        where: {
+          id: selectedCategoryIds,
+        },
+      });
+      // Associated new sighting with selected categories
+      await newSighting.setCategories(selectedCategories);
+      // Respond with new sighting
+      return res.json(newSighting);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
     }
   };
 
