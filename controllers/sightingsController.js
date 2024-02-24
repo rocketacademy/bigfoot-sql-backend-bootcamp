e709396728ed31ac04cb46d06776bb6e0fd444ff
Mannushka <zaimar1910@gmail.com>
2024-02-24 22:21:16 +0800

    create route to add a new sighting

--- controllers/sightingsController.js
+++ controllers/sightingsController.js
@@ -15,6 +15,23 @@ class SightingsController extends BaseController {
       return res.status(400).json({ error: true, msg: err });
     }
   }
+
+  //Post a new sighting
+  async addNewSighting(req, res) {
+    try {
+      const newSighting = await this.model.create({
+        date: req.body.date,
+        location: req.body.location,
+        notes: req.body.notes,
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+
+      return res.json(newSighting);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
 }
 
 module.exports = SightingsController;
--- db/models/index.js
+++ db/models/index.js
@@ -1,10 +1,10 @@
-'use strict';
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+"use strict";
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV;
-const config = require('../../config/database')[env];
+const config = require("../../config/database")[env];
 const db = {};
 
 let sequelize;
