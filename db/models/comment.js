5eb477c87efe68b2fe5fd0387ed7c57a2318a111
Mannushka <zaimar1910@gmail.com>
2024-02-28 20:51:30 +0800

    create comments model and table

--- db/models/comment.js
+++ db/models/comment.js
@@ -1,7 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Sighting extends Model {
+  class Comment extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -9,20 +9,23 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.comment);
+      Comment.belongsTo(models.sighting);
     }
   }
-  Sighting.init(
+  Comment.init(
     {
-      date: DataTypes.DATE,
-      location: DataTypes.STRING,
-      notes: DataTypes.TEXT,
+      content: DataTypes.STRING,
+      sighting_id: DataTypes.INTEGER,
+      references: {
+        model: "sightings",
+        key: "id",
+      },
     },
     {
       sequelize,
-      modelName: "sighting",
+      modelName: "comment",
       underscored: true,
     }
   );
-  return Sighting;
+  return Comment;
 };
