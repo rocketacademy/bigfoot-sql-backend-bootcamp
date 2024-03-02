d9d19188d71cbf6ad61722e4e2fab9a9f32db373
ianlauyin <ianlauyin@gmail.com>
2024-03-02 10:30:03 +0800

    Updated migration down function

--- db/migrations/20231226084438-create-sighting-category.js
+++ db/migrations/20231226084438-create-sighting-category.js
@@ -6,6 +6,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("sighting_categories");
+    await queryInterface.removeTable("sighting_categories", "intensity");
   },
 };
