3c4824e2d36df7d73ce80da0b24150de82f7c5ee
Mannushka <zaimar1910@gmail.com>
2024-03-07 22:14:02 +0800

    add middleware for categoriesRouter

--- index.js
+++ index.js
@@ -8,15 +8,21 @@ const SightingsRouter = require("./routers/sightingsRouter");
 // importing Controllers
 const SightingsController = require("./controllers/sightingsController");
 
+const CategoriesRouter = require("./routers/categoriesRouter");
+const CategoriesController = require("./controllers/categoriesController");
+
 // importing DB
 const db = require("./db/models/index");
-const { comment, sighting } = db;
+// const category = require("./db/models/category");
+const { comment, sighting, category } = db;
 
 // initializing Controllers -> note the lowercase for the first word
 const sightingsController = new SightingsController(sighting, comment);
+const categoriesController = new CategoriesController(category);
 
 // inittializing Routers
 const sightingRouter = new SightingsRouter(sightingsController).routes();
+const categoriesRouter = new CategoriesRouter(categoriesController).routes();
 
 const PORT = process.env.PORT;
 const app = express();
@@ -27,6 +33,7 @@ app.use(cors());
 
 // using the routers
 app.use("/sightings", sightingRouter);
+app.use("/categories", categoriesRouter);
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
