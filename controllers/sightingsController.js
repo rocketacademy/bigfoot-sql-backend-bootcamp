09bb1f5f90abecae8a872b4d4c9c0d62093ee946
Mannushka <zaimar1910@gmail.com>
2024-03-09 20:12:29 +0800

    add categoryModel into constructor

--- controllers/sightingsController.js
+++ controllers/sightingsController.js
@@ -1,16 +1,37 @@
 const BaseController = require("./baseController");
 
 class SightingsController extends BaseController {
-  constructor(model, commentModel) {
+  constructor(model, commentModel, categoryModel) {
     super(model);
     this.commentModel = commentModel;
+    this.categoryModel = categoryModel;
   }
 
+  async getAll(req, res) {
+    const location = req.query.location;
+    try {
+      let sightings = await this.model.findAll({ include: this.categoryModel });
+
+      if (location) {
+        sightings = sightings.filter(
+          (sighting) =>
+            sighting.location &&
+            sighting.location.toLowerCase().includes(location.toLowerCase())
+        );
+      }
+
+      return res.json(sightings);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
   // Retrieve specific sighting
   async getOne(req, res) {
     const { sightingId } = req.params;
     try {
-      const sighting = await this.model.findByPk(sightingId);
+      const sighting = await this.model.findByPk(sightingId, {
+        include: this.categoryModel,
+      });
       return res.json(sighting);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
@@ -19,13 +40,22 @@ class SightingsController extends BaseController {
 
   //Post a new sighting
   async postNewSighting(req, res) {
-    const { date, location, notes } = req.body;
+    const { date, location, notes, selectedCategoryIds } = req.body;
     try {
       const newSighting = await this.model.create({
         date: date,
         location: location,
         notes: notes,
+        selectedCategoryIds,
       });
+      // Retrieve selected categories
+      const selectedCategories = await this.categoryModel.findAll({
+        where: {
+          id: selectedCategoryIds,
+        },
+      });
+      // Associated new sighting with selected categories
+      await newSighting.setCategories(selectedCategories);
       return res.json(newSighting);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
