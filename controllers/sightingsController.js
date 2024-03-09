bcab2d2d3b51341857ffe9a218d537641b1e7da5
magiicloud <hongyun_rc@outlook.com>
2024-03-09 22:47:27 +0800

    amended controller to include categories

--- controllers/sightingsController.js
+++ controllers/sightingsController.js
@@ -3,18 +3,32 @@ const comment = require("../db/models/comment");
 const BaseController = require("./baseController");
 
 class SightingsController extends BaseController {
-  constructor(sightingModel, commentModel) {
+  constructor(sightingModel, commentModel, categoryModel) {
     super(sightingModel);
     this.sightingModel = sightingModel;
     this.commentModel = commentModel;
+    this.categoryModel = categoryModel;
   }
 
   // Retrieve specific sighting
   async getOne(req, res) {
     const { sightingId } = req.params;
     try {
-      const sighting = await this.model.findByPk(sightingId);
+      const sighting = await this.model.findByPk(sightingId, {
+        include: this.categoryModel,
+      });
       return res.json(sighting);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+
+  async getAll(req, res) {
+    try {
+      const output = await this.model.findAll({
+        include: this.categoryModel,
+      });
+      return res.json(output);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
     }
@@ -22,12 +36,23 @@ class SightingsController extends BaseController {
 
   //Create sighting
   async add(req, res) {
-    let sightingToAdd = req.body;
+    const { date, location, notes, selectedCategoryIds } = req.body;
     try {
-      let sightingAdded = await this.model.create(sightingToAdd);
+      let sightingAdded = await this.model.create({
+        date: new Date(date),
+        location: location,
+        notes: notes,
+      });
+      const selectedCategories = await this.categoryModel.findAll({
+        where: {
+          id: selectedCategoryIds,
+        },
+      });
+      await sightingAdded.setCategories(selectedCategories);
       return res.json(sightingAdded);
     } catch (err) {
-      return res.status(400).json({ error: true, msg: err });
+      console.error(err);
+      return res.status(400).json({ error: true, msg: err.message });
     }
   }
 
